d6cf0b36a97b5d1110895cc55c9392e1970cf568
Aviral Srivastava <srivastavaaviral1@gmail.com>
2023-06-16 13:57:39 +0530

    feat: added search functionality

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Button from "@material-ui/core/ButtonBase"
+import TextField from "@material-ui/core/TextField"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
-import { Person } from "shared/models/person"
+import { Person, PersonHelper } from "shared/models/person"
 import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
 
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
+  const [searchText, setSearchText] = useState<string>("")
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
 
   useEffect(() => {
@@ -33,7 +35,7 @@ export const HomeBoardPage: React.FC = () => {
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction} setSearchText={setSearchText} searchText={searchText} />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -43,9 +45,11 @@ export const HomeBoardPage: React.FC = () => {
 
         {loadState === "loaded" && data?.students && (
           <>
-            {data.students.map((s) => (
-              <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
-            ))}
+            {data.students.map((s: Person) => {
+              if (PersonHelper.getFullName(s).toUpperCase().includes(searchText.toUpperCase())) {
+                return <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
+              }
+            })}
           </>
         )}
 
@@ -63,13 +67,24 @@ export const HomeBoardPage: React.FC = () => {
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
   onItemClick: (action: ToolbarAction, value?: string) => void
+  setSearchText: (searchText: string) => void
+  searchText: string
 }
+
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, setSearchText, searchText } = props
   return (
     <S.ToolbarContainer>
       <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <S.StyledTextField
+        className="searchTextField"
+        label="Search"
+        variant="standard"
+        value={searchText}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setSearchText(event.target.value)
+        }}
+      />
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
@@ -91,6 +106,24 @@ const S = {
     padding: 6px 14px;
     font-weight: ${FontWeight.strong};
     border-radius: ${BorderRadius.default};
+  `,
+  StyledTextField: styled(TextField)`
+    && {
+      color: #fff;
+      /* border: 1px solid red; */
+      .MuiFormLabel-root {
+        color: #fff;
+      }
+      .MuiInputBase-root {
+        border-radius: 4px;
+        padding: ${Spacing.u1};
+        margin-top: 0px;
+        color: #fff;
+      }
+      .MuiInputBase-input {
+        /* padding: ${Spacing.u2}; */
+      }
+    }
   `,
   Button: styled(Button)`
     && {
